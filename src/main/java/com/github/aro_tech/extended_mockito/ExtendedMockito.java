644cd6ffe625856b2f6376c49d5dcab5f15334af
aro_tech <aro_tech@gmx.com>
2016-05-28 09:33:53 +0200

    Removed stubVoid() fix, because stubVoid() was removed from Mockito in 2.54

--- src/main/java/com/github/aro_tech/extended_mockito/ExtendedMockito.java
+++ src/main/java/com/github/aro_tech/extended_mockito/ExtendedMockito.java
@@ -4,8 +4,6 @@
 package com.github.aro_tech.extended_mockito;
 
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mockito;
-import org.mockito.stubbing.VoidMethodStubbable;
 
 /**
  * Mixin interface which includes Mockito plus added functionalities
@@ -14,25 +12,6 @@ import org.mockito.stubbing.VoidMethodStubbable;
  *
  */
 public interface ExtendedMockito extends MockitoMixin, ExtendedMatchers {
-	/**
-	 * Delegate call to public static
-	 * <T> org.mockito.stubbing.VoidMethodStubbable
-	 * <T> org.mockito.Mockito.stubVoid(T)
-	 * 
-	 * Overridden here as deprecated because in the auto-generated MockitoMixin
-	 * class it's not annotated as deprecated. That's because in Mockito itself
-	 * the method is not properly annotated as deprecated.
-	 * 
-	 * {@link org.mockito.Mockito#stubVoid(java.lang.Object)}
-	 * 
-	 * @deprecated Use {@link ExtendedMockito#doThrow(Throwable...)} method for
-	 *             stubbing voids
-	 */
-	@Override
-	@Deprecated
-	default <T> VoidMethodStubbable<T> stubVoid(T mock) {
-		return Mockito.stubVoid(mock);
-	}
 
 	/**
 	 * Convenience method for creating an ArgumentCaptor for the given type
